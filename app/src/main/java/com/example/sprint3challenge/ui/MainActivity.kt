c467dc7d45e7192e057412aed17b35905e4cca85
victor6021 <victor60216021@gmail.com>
2019-08-23 11:01:15 -0700

    adding animated vector image

--- app/src/main/java/com/example/sprint3challenge/ui/MainActivity.kt
+++ app/src/main/java/com/example/sprint3challenge/ui/MainActivity.kt
@@ -1,12 +1,19 @@
-package com.example.sprint3challenge
+package com.example.sprint3challenge.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.sprint3challenge.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button_add.setOnClickListener {
+
+        }
     }
+
 }
