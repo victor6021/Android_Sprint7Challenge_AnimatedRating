0d776798bc07a25408f377ce83680d388fc96c7f
victor6021 <victor60216021@gmail.com>
2019-08-23 10:10:57 -0700

    finished main page

--- app/src/androidTest/java/com/example/sprint3challenge/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/sprint3challenge/ExampleInstrumentedTest.kt
@@ -7,6 +7,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import androidx.test.runner.AndroidJUnit4 as AndroidJUnit4
 
 /**
  * Instrumented test, which will execute on an Android device.
